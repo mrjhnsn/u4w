ee262275e22a086ca7a91ca8265942012b553961
mrjhnsn <trevor@mrjhnsn.com>
2018-06-10 18:52:11 -0700

    updated text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,32 +26,32 @@ const AboutSection = () => (
     <div className="row column large-9 xlarge-6 xxlarge-4">
       <h2 className="section-title">How Does This Work?</h2>
       <p className="content">
-        In today’s fast paced world, you’ve got places you need to be but not enough time in your jam packed schedule. Wouldn’t it be nice if there were a transportation service that changed the way you get around daily? Introducing Wild Rydes, an innovative transportation service that helps people get to their destination faster and hassle-free. Getting started is as easy as tapping a button in our app.
+        Seriously, why do all websites need some BS block of buzzword text. We are here to make sure you can quickly and legally order marijuana for delivery. Why wait to get high? The on-demand dealers of old are gone. Time to get with the new generation and order your weed through an app just like you do everything else.
       </p>
     </div>
     <div className="row medium-up-2 large-up-4">
       <div className="column">
         <div className="home-about-block">
           <h3 className="title icon-download">Download The App</h3>
-          <p className="content">Head over to the app store and download the Wild Rydes app. You’re just a few taps away from getting your ryde.</p>
+          <p className="content">Head over to the app store and download the U4W app. You’re just a few taps away from getting your ass high as fuck.</p>
         </div>
       </div>
       <div className="column">
         <div className="home-about-block">
-          <h3 className="title icon-unicorn">Request A Unicorn</h3>
-          <p className="content">We can get you there. Simply request a ryde on the app and we'll connect you with a unicorn immediately.</p>
+          <h3 className="title icon-unicorn">Request your Sack</h3>
+          <p className="content">We can get you high wherever you are. Simply request the dank on the app and we'll connect you with a mobile dispensary immediately.</p>
         </div>
       </div>
       <div className="column">
         <div className="home-about-block">
-          <h3 className="title icon-price">Pick A Price</h3>
-          <p className="content">Pick the valuation you're willing to pay and your ryde is set up. The only surge is the acceleration you get when taking off.</p>
+          <h3 className="title icon-price">Pay some money</h3>
+          <p className="content">The money you pay will go to helping farmers send their kids to college and pay for the stuff you exepect in the overhead costs it takes to deliver the weed to you. </p>
         </div>
       </div>
       <div className="column">
         <div className="home-about-block">
-          <h3 className="title icon-success">Ride Off To Success!</h3>
-          <p className="content">After matching with your unicorn and agreeing to its terms, you’ll be all set. Your unicorn will arrive shortly to pick you up.</p>
+          <h3 className="title icon-success">Smoke it out!</h3>
+          <p className="content">After you have selected the products you would like delivered we will rapidly depoly a licensed dispensary driver to deliver your weed products of choice.</p>
         </div>
       </div>
     </div>
@@ -63,7 +63,7 @@ const StorySection = () => (
     <div className="row column large-9 xlarge-6 xxlarge-4">
       <h2 className="section-title">Our Story</h2>
       <p className="content">
-        Wild Rydes was started by a former hedge fund analyst and a software developer. The two long-time friends happened upon the Wild Rydes idea after attending a silent yoga retreat in Nevada. After gazing upon the majestic herds of unicorns prancing across a surreal Nevada sunset, they witnessed firsthand the poverty and unemployment endemic to that once proud race. Whether it was modern society’s reliance on science over magic or not, we’ll never know the cause of their Ozymandian downfall and fade to obscurity. Moved by empathy, romance, and free enterprise, they saw an opportunity to marry society’s demand for faster, more flexible transportation to underutilized beasts of labor through an on-demand market making transportation app. Using the founders’ respective expertise in animal husbandry and software engineering, Wild Rydes was formed and has since raised untold amounts of venture capital. Today, Wild Rydes has thousands of unicorns in its network fulfilling hundreds of rydes each day.
+        U4W was started by a random dude and his buddy, a software developer. The two long-time friends happened upon the U4W idea after seeing how much money venture capitalists are throwing at bullshit projects with no vision or focus. The marijuana industry is only expanding nationwide and to succeed in that vertical, tech companies should be focusing on building a lasting framework for the business ranther than trying to quickly cash out. Using the founders’ respective expertise in, weed smoking, taxi driving, systems administration and software engineering, U4W was formed and has since raised fuckloads of venture capital that they are definitely not spending on cocaine and hookers (they are too old for that shit).
       </p>
     </div>
   </section>
@@ -94,9 +94,9 @@ const QuoteSection = () => (
     <div className="row column medium-8 xxlarge-6">
       <div className="quote-wrap">
         <div className="quote">
-          “I was almost late to my ultimate frisbee tournament in DOLORES park. BUt Wild Rydes Got me there from the marina in under five minutes.”
+          “I was almost late to my ultimate frisbee tournament in DOLORES park. BUt U4W Got me there from the marina in under five minutes.”
         </div>
-        <div className="quoter">- Satisfied Wild Rydes User</div>
+        <div className="quoter">- Satisfied U4W User</div>
       </div>
     </div>
   </section>
@@ -111,7 +111,7 @@ const KrakenSection = () => (
       <div className="columns medium-6 xlarge-5 xxlarge-4">
         <h4 className="title">Coming Soon</h4>
         <p className="content">
-          Kraken 3xplorer and Dragon Flyght. We’re looking to provide you a full service package from air, land, and to sea.
+          Some cool stuff for the future here.
         </p>
       </div>
     </div>
@@ -122,7 +122,7 @@ const Home = () => {
   return (
     <div className="page-home">
       <header className="site-header">
-        <h1 className="title">Wild Rydes</h1>
+        <h1 className="title">U4W</h1>
         <SiteNav/>
         <Link className="home-button" to="/app">
           Giddy Up!
